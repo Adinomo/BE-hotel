4556a5eb94932e1ed470f97c71c04b457b054463
Adinomo <adinomo58@gmail.com>
2025-11-24 23:29:30 +0700

    Fix DB env names

--- db.js
+++ db.js
@@ -1,11 +1,11 @@
 const mysql = require("mysql2");
 
 const db = mysql.createConnection({
-  host: "containers-us-west-151.railway.app",
-  port: 6738,
-  user: "root",
-  password: "OmzuTGVjyxYKmjbDzmWvoYFgOLzYXZyc",
-  database: "railway"
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,   // <-- ganti ini
+  database: process.env.DB_NAME,
 });
 
 db.connect(err => {
